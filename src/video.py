3e747ece4282fe7fc7866b1c9c8d3169a1300049
Oleg <larog02@mail.ru>
2024-07-29 22:34:26 +0300

    home6 done

--- src/video.py
+++ src/video.py
@@ -1,21 +1,39 @@
 import os
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 import isodate
 
 class Video:
     api_key: str = os.getenv("API_KEY")
     youtube = build('youtube', 'v3', developerKey=api_key)
     
-    def __init__(self, channel_id: str) -> None:
-        video_response = Video.youtube.videos().list(part='snippet,statistics',
-                                                     id=channel_id
-                                                     ).execute()
+    def __init__(self, video_id: str) -> None:
 
-        self.id = channel_id
-        self.title = video_response['items'][0]['snippet']['title']
-        self.url = f"https://www.youtube.com/watch?v={channel_id}"
-        self.count_views = video_response['items'][0]['statistics']['viewCount']
-        self.count_likes = video_response['items'][0]['statistics']['likeCount']
+        self.id = video_id
+        self.title = None
+        self.url = None
+        self.count_views = None
+        self.like_count = None
+
+        try:
+            # Запрашиваем данные о видео с помощью API
+            video_response = Video.youtube.videos().list(
+                part='snippet,statistics',
+                id=video_id
+            ).execute()
+
+            # Проверяем, что видео найдено
+            if video_response['items']:
+                self.title = video_response['items'][0]['snippet']['title']
+                self.url = f"https://www.youtube.com/watch?v={video_id}"
+                self.count_views = video_response['items'][0]['statistics']['viewCount']
+                self.like_count = video_response['items'][0]['statistics']['likeCount']
+            else:
+                # Если видео не найдено, оставляем атрибуты в значении None
+                pass
+        except Exception as e:
+            # Ловим любые исключения и оставляем атрибуты в значении None
+            print(f"Ошибка при запросе данных о видео: {e}")
 
     def __str__(self):
         return f'{self.title}'
